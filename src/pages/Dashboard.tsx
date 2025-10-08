a9e89a8be4da9afb8d2078ebea222494eccde52a
Jörgen Thelin <jorgen.thelin@monitum.io>
2025-10-08 14:16:18 +0200

    dashboard layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,16 +12,15 @@ const Dashboard = () => {
       <SegmentSelector />
       <SegmentProgress />
 
-      <main className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        {/* Left Column */}
-        <div className="lg:col-span-1 space-y-8">
+      <main className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div>
           <SessionLogger />
-          <Metronome />
         </div>
-
-        {/* Right Column */}
-        <div className="lg:col-span-2 space-y-8">
+        <div>
           <TodaysSessions />
+        </div>
+        <div>
+          <Metronome />
         </div>
       </main>
     </>
